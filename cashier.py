05c4145244ae2644fb418fc30d9a5f7b4c3c04b8
Ghad1ee2r3 <52378925+Ghad1ee2r3@users.noreply.github.com>
2020-08-18 10:55:53 +0300

    Update cashier.py

--- cashier.py
+++ cashier.py
@@ -1,5 +1,51 @@
 def main():
 	# write code here
+	list=[]
+	name=""
+	while name!="done":
+		name=input("Item (enter done when finished):")
+		if name =="done":
+			break
+		else:
+			price=input("Price:")
+			quantity=input("Quantity:")
+			#list=[]
+			d={"quantity":quantity,"name":name ,"price":price }
+			list.append(d)
+		#dictionary_copy = d.copy()
+		#list.append(dictionary_copy)
+
+	#print(list)
+	#print(list[])
+	#-------------------
+	print ("-------------------")
+	print("receipt")
+	print ("-------------------")
+	p=0
+	for i in list:
+		#j=1
+		#for j in i:
+			#print(j)
+			#if j==0:
+		p=p+int(i["quantity"])*int(i["price"])
+		p1=int(int (i["quantity"])*int(i["price"]))
+		print(i["quantity"]+". "+i["name"]+"  "+str(p1)+"KD" )
+
+		#	else:
+		#		break
+	print ("-------------------")
+	print ("Total Price:"+str(p)+"KD")
+		#print(q , name , q*price)
+		#print(ii)
+		#print(i)
+	#for key,values in list.items():
+	#	for v in values:
+		#	print(key," : ",v)
+		#for k in dictionary:
+    #print("key: %s" % k)
+    #print("value: %s" % dictionary[k])
+
+		#pass
 
 if __name__ == '__main__':
 	main()
